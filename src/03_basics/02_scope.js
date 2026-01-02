9e0419d19bea641d703facfe618c0c058fec4e39
rakeshweb7776 <rakeshweb7776@gmail.com>
2026-01-02 21:17:26 +0530

    scope learning

--- src/03_basics/02_scope.js
+++ src/03_basics/02_scope.js
@@ -11,10 +11,47 @@ export function scope_1() {
         // Local Scope
         let a = 50
         const b = 20
-        console.log(20);
-
+        // console.log(20);
     }
-    console.log(a);
+    // console.log(a);
+
+
+
+    function one() {
+        const username = "Rakesh"
+        function two() {
+            const websiteName = "youtube"
+            console.log(`Username is ${username} and website is ${websiteName}`);
+        }
+        two()
+        // console.log(websiteName);
+    }
+    one()
+
+
+    if (true) {
+        const user = "Rakesh"
+        if (user === "Rakesh") {
+            const website = "youtube"
+            console.log(`Username is ${user} and website is ${website}`);
+        }
+        // console.log(website); // out of scope
+    }
+    // console.log(user); // out of scope
+
+
+
+    addone(5) // this type of function is we can call
+    function addone(num) {
+        return num + 1
+    }
+
+    addVal(5) // if we contain function in a variable then we can not call it like this.
+    const addVal = function(num){
+        return num + 1
+    }
+
+
 
 
 }
