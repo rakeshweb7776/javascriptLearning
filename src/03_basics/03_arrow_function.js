d6ee1e258360cda4ef2f703907a54ccacb7f7eb5
rakeshweb7776 <rakeshweb7776@gmail.com>
2026-01-03 10:16:13 +0530

    arrow function

--- src/03_basics/03_arrow_function.js
+++ src/03_basics/03_arrow_function.js
@@ -1,5 +1,54 @@
 export function arrow_function() {
 
+
+    // Call a function from object 
+    const user = {
+        username: "rakesh",
+        price: 999,
+        welcomeMessage: function () {
+            console.log(`${this.username},Welcome to website`);
+        }
+    }
+
+    // user.welcomeMessage()
+    // user.username = "Hitesh"
+    // user.welcomeMessage()
+
+
+    // function checkThis(){
+    //     let username = "hitesh"
+    //     console.log(this.username)
+    // }
+    // checkThis()
+
+
+    // const checkThis = function() {
+    //     let username = "hitesh"
+    //     console.log(this.username)
+    // }
+    // checkThis()
+
+
+    // const checkThis = () => {
+    //     let userData = {
+    //         name:"rakesh",
+    //         email:"rakesh@gmail.com"
+    //     }
+    // }
+
+    // checkThis()
+
+
+    // const addTwo = (num1, num2) => {
+    //     return num1 + num2 // Explicit returns
+    // }
     
+    
+    const addTwo = (num1, num2) => (num1 + num2)  // Implicit returns
+
+    console.log(addTwo(4, 5))
+    
+
+
 
 }
